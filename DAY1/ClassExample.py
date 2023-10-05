48e13b140c18f8b7d6b4f1b44cac0f5b4374a2af
2103A51176 <108998172+2103A51176@users.noreply.github.com>
2023-10-05 09:21:20 +0530

    Update ClassExample.py

---
+++ DAY1/ClassExample.py
@@ -0,0 +1,18 @@
+class name:
+    def first():
+        print("first")
+    def second():
+        print("second")
+
+name.first()
+
+#class with making object
+class Name:
+    def first(self):
+        print("first")
+    def second(self):
+        print("second")
+
+c = Name()
+d = Name()
+print(c==d)
